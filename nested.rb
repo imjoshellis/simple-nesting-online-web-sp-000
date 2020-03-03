2b634810da904acfe326e27791105087ebd1bcc0
Josh Ellis <joshe523@gmail.com>
2020-03-03 06:09:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,6 +90,7 @@ def adding_matz
      }
 end
 
+require "pry"
 def changing_alan
 	programmer_hash =
  		{
@@ -107,7 +108,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+binding.pry
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
 end
